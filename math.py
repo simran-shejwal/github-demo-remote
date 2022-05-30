38e2247f164fba3b4c1f380da8c3350e4cfe181c
simran-shejwal <106535786+simran-shejwal@users.noreply.github.com>
2022-05-30 18:43:52 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+// Add implementation
+def add(x, y):
+	return x+y
+
+// Substract implementation
+def substract(x, y):
+	return x-y		//on master branch
+
+// Multiply implementation
+def multiply(x,y):
+	return x*y		//on Bug456 branch
+
+// Divide implementation
+def divide(x,y):
+	if y==0:		//on master branch
+		return DIVIDE_BY_0_ERROR
+	else
+		return x/y
+
+// Square implementation 
+def square(x):
+	return x*x
